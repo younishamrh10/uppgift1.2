a6d4fc142b549b46b41eddf7dd5061a913a7b4c0
yoha06001 <yoha06001@E-PW0234KT>
2023-09-06 10:41:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift2
+{
+    class Ptogram
+    {
+        static void Main(string[]args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Din dator är krascha för att den fick vatten i sig"); 
+
+        }
+    }
+}
